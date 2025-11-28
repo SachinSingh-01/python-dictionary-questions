9169e224ff84173fe837f76f8a0907155043ca64
Sachin Singh <sachinsingh19012@gmail.com>
2025-11-28 21:37:54 +0530

    Add more question

---
+++ 05_problem.py
@@ -0,0 +1,13 @@
+'''Create a dictionary containing:
+string value
+integer value
+list value
+boolean value
+Print the dictionary.'''
+data={
+    "string_value":"Sachin Singh",
+    "integer_value":(1,3,4,5,6,6),
+    "list_value":[1,4,7,4,2,8],
+    "boolean_value":(True,False)
+}
+print(data)
---
+++ 06_problem.py
@@ -0,0 +1,39 @@
+# Use get() to access the value of key "year" from a dictionary.
+car={
+    "brand":"Toyota",
+    "model":"Toyota hilux",
+    "year":1970
+}
+print(car)
+x=car["year"]
+print(x)
+
+# Create a dictionary and print all keys using keys() method.
+car={
+    "brand":"Toyota",
+    "model":"Toyota hilux",
+    "year":1970
+}
+print(car)
+x=car.keys()
+print(x)
+
+# Create a dictionary and print all values using values() method.
+car={
+    "brand":"Toyota",
+    "model":"Toyota hilux",
+    "year":1970
+}
+print(car)
+x=car.values()
+print(x)
+
+# Create a dictionary and print all key-value pairs using items()
+car={
+    "brand":"Toyota",
+    "model":"Toyota hilux",
+    "year":1970
+}
+print(car)
+x=car.items()
+print(x)
