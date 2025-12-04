f0e11a3925ac5c978ffcb836c46a550a7a99a305
Sachin Singh <sachinsingh19012@gmail.com>
2025-12-04 22:31:10 +0530

    Dictionary practice question

--- 41_problem.py
+++ 41_problem.py
@@ -1,1 +1,10 @@
 # Swap keys and values of a dictionary (values will be unique).
+d = {
+    "name": "sachin",
+    "age": 20,
+    "city": "delhi"
+}
+swapped = {}
+for k, v in d.items():
+    swapped[v] = k
+print(swapped)
